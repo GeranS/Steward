1e22da9333e00a4e362895838161af90f7f2c96a
MrGrindor <54775642+MrGrindor@users.noreply.github.com>
2020-05-28 15:04:15 +0200

    updated weapons and roll services to work with new weapon attributes

---
+++ Steward/Discord/GenericCommands/ValkFinderInventoryModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Steward.Context;
+using Steward.Context.Models;
+using Steward.Discord.CustomPreconditions;
+using Steward.Services;
+
+namespace Steward.Discord.GenericCommands
+{
+    public class ValkFinderInventoryModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly StewardContext _stewardContext;
+        private readonly InventoryService _inventoryService;
+        public ValkFinderInventoryModule(StewardContext c, InventoryService i)
+        {
+            _inventoryService = i;
+            _stewardContext = c;
+        }
+
+        [Command("add item")]
+        [RequireStewardPermission]
+        public async Task addItem(string ItemID, [Remainder]SocketGuildUser mention)//add item to mentioned player 
+        {
+
+        }
+    }
+}
