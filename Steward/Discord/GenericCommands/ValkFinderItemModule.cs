54db7ecae0acda9c298bfaf1c05b766972fa6ff5
MrGrindor <54775642+MrGrindor@users.noreply.github.com>
2020-05-28 17:14:55 +0200

    Added Item Module

---
+++ Steward/Discord/GenericCommands/ValkFinderItemModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Microsoft.EntityFrameworkCore;
+using Steward.Context;
+using Steward.Context.Models;
+using Steward.Discord.CustomPreconditions;
+using Steward.Services;
+
+
+namespace Steward.Discord.GenericCommands
+{
+    public class ValkFinderItemModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly StewardContext _stewardContext;
+
+        public ValkFinderItemModule(StewardContext c)
+        {
+            _stewardContext = c;
+        }
+
+
+    }
+}
