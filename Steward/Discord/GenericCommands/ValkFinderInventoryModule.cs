f516dae7d720d05852335c5e90adc3bdbfbc2c45
MrGrindor <54775642+MrGrindor@users.noreply.github.com>
2020-05-28 20:06:40 +0200

    update to inventory
    user command for transfering items between users.
    admin commands for granting and taking item

--- Steward/Discord/GenericCommands/ValkFinderInventoryModule.cs
+++ Steward/Discord/GenericCommands/ValkFinderInventoryModule.cs
@@ -9,6 +9,7 @@ using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using Steward.Context;
 using Steward.Context.Models;
 using Steward.Discord.CustomPreconditions;
@@ -20,20 +21,44 @@ namespace Steward.Discord.GenericCommands
     {
         private readonly StewardContext _stewardContext;
         private readonly InventoryService _inventoryService;
-        public ValkFinderInventoryModule(StewardContext c, InventoryService i)
+        private readonly DiscordSocketClient _client;
+        public ValkFinderInventoryModule(StewardContext c, InventoryService i, DiscordSocketClient cl)
         {
             _inventoryService = i;
             _stewardContext = c;
+            _client = cl;
         }
 
         [Command("give")]
-        public async Task addItem(string itemName, string type, [Remainder]SocketGuildUser mention)//add item to mentioned player 
+        public async Task transfer(string itemName, string type, [Remainder]SocketGuildUser mention, int amount = 1)//add item to mentioned player 
         {
             //check wheter ItemID of type exists
-            //check wheter player has item or is admin
+            //check wheter player has item 
             //give player item
-            
-            /*switch (type)
+            var commandUser = _stewardContext.DiscordUsers
+                    .Include(du => du.Characters)
+                    .SingleOrDefault(u => u.DiscordId == Context.User.Id.ToString());
+
+            var activeCharacter = commandUser.Characters.Find(c => c.IsAlive());
+
+            if (activeCharacter == null)
+            {
+                await ReplyAsync("Could not find a living character.");
+                return;
+            }
+            var discordUser = _stewardContext.DiscordUsers
+                    .Include(du => du.Characters)
+                    .SingleOrDefault(u => u.DiscordId == mention.Id.ToString());
+
+            var recievingCharacter = discordUser.Characters.Find(c => c.IsAlive());
+
+            if (recievingCharacter == null)
+            {
+                await ReplyAsync($"Could not find a living character for {_client.GetUser(ulong.Parse(discordUser.DiscordId)).ToString()}.");
+                return;
+            }
+            object item;
+            switch (type)
             {
                 case "weapon":
                     item = _stewardContext.ValkFinderWeapons.FirstOrDefault(w => w.WeaponName == itemName);
@@ -47,7 +72,142 @@ namespace Steward.Discord.GenericCommands
                 default:
                     await ReplyAsync($"{type} is not a valid item type. Types are armour/weapon/item.");
                     return;
-            }*/
+            }
+            if (item == null)
+            {
+                await ReplyAsync($"{itemName} either does not exist or is not a {type}");
+                return;
+            }
+
+            //check for items in sender inv
+            var senderInv = _stewardContext.CharacterInventories.FirstOrDefault(i => i.PlayerCharacterId == activeCharacter.CharacterId && (i.ValkFinderArmour == item || i.ValkFinderWeapon == item || i.ValkFinderItem == item));
+
+            //does sender have enough items?
+            if (senderInv == null || (senderInv.Amount <= amount))
+            {
+                await ReplyAsync($"You don't have enough of {itemName} in you inventory to give {amount} away");
+                return;
+            }
+
+            await _inventoryService.GiveItem(recievingCharacter, item, amount, type); //gives item to reciever
+
+            await _inventoryService.TakeItem(senderInv, item, amount, type); //takes item with sender inventory
+
+            await ReplyAsync($"{amount} of the {type} {itemName} has been given to {_client.GetUser(ulong.Parse(discordUser.DiscordId)).ToString()}");
+        }
+
+        [Command("inventory")]
+        public async Task showInventory([Remainder]SocketGuildUser mention = null)
+        {
+            DiscordUser user = null;
+
+            if (mention != null)
+            {
+                user = _stewardContext.DiscordUsers
+                    .Include(du => du.Characters)
+                    .SingleOrDefault(u => u.DiscordId == mention.Id.ToString());
+            }
+            else
+            {
+                user = _stewardContext.DiscordUsers
+                    .Include(du => du.Characters)
+                    .SingleOrDefault(u => u.DiscordId == Context.User.Id.ToString());
+            }
+
+            var activeCharacter = user.Characters.FirstOrDefault(c => c.IsAlive());
+
+            if (activeCharacter == null)
+            {
+                await ReplyAsync("Could not find a living character.");
+                return;
+            }
+
+            var embedBuilder = _inventoryService.createInventory(activeCharacter);
+            await ReplyAsync(embed: embedBuilder.Build());
+            
+        }
+
+        [Command("grant item")]
+        [RequireStewardPermission]
+        public async Task grantItem(string itemName, string type, [Remainder]SocketGuildUser mention, int amount = 1)
+        {
+            var discordUser = _stewardContext.DiscordUsers
+                    .Include(du => du.Characters)
+                    .SingleOrDefault(u => u.DiscordId == mention.Id.ToString());
+
+            var recievingCharacter = discordUser.Characters.Find(c => c.IsAlive());
+
+            if (recievingCharacter == null)
+            {
+                await ReplyAsync($"Could not find a living character for {_client.GetUser(ulong.Parse(discordUser.DiscordId)).ToString()}.");
+                return;
+            }
+            object item;
+            switch (type)
+            {
+                case "weapon":
+                    item = _stewardContext.ValkFinderWeapons.FirstOrDefault(w => w.WeaponName == itemName);
+                    break;
+                case "armour":
+                    item = _stewardContext.ValkFinderArmours.FirstOrDefault(w => w.ArmourName == itemName);
+                    break;
+                case "item":
+                    item = _stewardContext.ValkFinderItems.FirstOrDefault(w => w.ItemName == itemName);
+                    break;
+                default:
+                    await ReplyAsync($"{type} is not a valid item type. Types are armour/weapon/item.");
+                    return;
+            }
+            if (item == null)
+            {
+                await ReplyAsync($"{itemName} either does not exist or is not a {type}");
+                return;
+            }
+
+            await _inventoryService.GiveItem(recievingCharacter, item, amount, type);
+            await ReplyAsync($"{amount} of the {type} {itemName} has been granted to {_client.GetUser(ulong.Parse(discordUser.DiscordId)).ToString()}");
+        }
+
+        [Command("take item")]
+        [RequireStewardPermission]
+        public async Task takeItem(string itemName, string type, [Remainder]SocketGuildUser mention, int amount = 1)
+        {
+            var discordUser = _stewardContext.DiscordUsers
+                    .Include(du => du.Characters)
+                    .SingleOrDefault(u => u.DiscordId == mention.Id.ToString());
+
+            var victimCharacter = discordUser.Characters.Find(c => c.IsAlive());
+
+            if (victimCharacter == null)
+            {
+                await ReplyAsync($"Could not find a living character for {_client.GetUser(ulong.Parse(discordUser.DiscordId)).ToString()}.");
+                return;
+            }
+            object item;
+            switch (type)
+            {
+                case "weapon":
+                    item = _stewardContext.ValkFinderWeapons.FirstOrDefault(w => w.WeaponName == itemName);
+                    break;
+                case "armour":
+                    item = _stewardContext.ValkFinderArmours.FirstOrDefault(w => w.ArmourName == itemName);
+                    break;
+                case "item":
+                    item = _stewardContext.ValkFinderItems.FirstOrDefault(w => w.ItemName == itemName);
+                    break;
+                default:
+                    await ReplyAsync($"{type} is not a valid item type. Types are armour/weapon/item.");
+                    return;
+            }
+            if (item == null)
+            {
+                await ReplyAsync($"{itemName} either does not exist or is not a {type}");
+                return;
+            }
+            var victimInv = _stewardContext.CharacterInventories.FirstOrDefault(i => i.PlayerCharacterId == victimCharacter.CharacterId && (i.ValkFinderArmour == item || i.ValkFinderWeapon == item || i.ValkFinderItem == item));
+
+            await _inventoryService.TakeItem(victimInv, item, amount, type);
+            await ReplyAsync($"{amount} of the {type} {itemName} has been taken from {_client.GetUser(ulong.Parse(discordUser.DiscordId)).ToString()}");
         }
     }
 }
--- Steward/Services/InventoryService.cs
+++ Steward/Services/InventoryService.cs
@@ -1,10 +1,124 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Steward.Context;
+using Steward.Context.Models;
+using Steward.Discord.CustomPreconditions;
+using Steward.Services;
 
 namespace Steward.Services
 {
     public class InventoryService
     {
+        StewardContext _stewardContext;
+
+        public InventoryService(StewardContext s)
+        {
+            _stewardContext = s;
+        }
+
+        public async Task GiveItem(PlayerCharacter reciever, Object item, int amount, string type)
+        {
+            var recieverInv = _stewardContext.CharacterInventories.FirstOrDefault(i => i.PlayerCharacterId == reciever.CharacterId && (i.ValkFinderArmour == item || i.ValkFinderWeapon == item || i.ValkFinderItem == item));
+            if (recieverInv == null)//he doesn't already have this type of item
+            {
+                recieverInv = new CharacterInventory()
+                {
+                    PlayerCharacterId = reciever.CharacterId,
+                    Amount = amount
+                };
+                switch (type)
+                {
+                    case "weapon":
+                        recieverInv.ValkFinderWeapon = item as ValkFinderWeapon;
+                        break;
+                    case "armour":
+                        recieverInv.ValkFinderArmour = item as ValkFinderArmour;
+                        break;
+                    case "item":
+                        recieverInv.ValkFinderItem = item as ValkFinderItem;
+                        break;
+                }
+                _stewardContext.CharacterInventories.Add(recieverInv);
+
+            }
+            else //he already has this type of item
+            {
+                recieverInv.Amount += amount; //give him more
+                _stewardContext.CharacterInventories.Update(recieverInv);
+            }
+            await _stewardContext.SaveChangesAsync();
+        }
+
+        public async Task TakeItem(CharacterInventory victimInv, object item, int amount, string type)
+        {
+            victimInv.Amount -= amount;
+            if (victimInv.Amount <= 0) //if its zero delete the inventory
+            {
+                _stewardContext.CharacterInventories.Remove(victimInv);
+            }
+            else //if its over zero update the inventory
+            {
+                _stewardContext.CharacterInventories.Update(victimInv);
+            }
+            await _stewardContext.SaveChangesAsync();
+        }
+
+        public EmbedBuilder createInventory(PlayerCharacter character)
+        {
+            var characterInventory = _stewardContext.CharacterInventories.ToList().Where(i => i.PlayerCharacterId == character.CharacterId);
+            var embedBuilder = new EmbedBuilder()
+            {
+                Color = Color.Purple,
+                Title = "Inventory"
+            };
+
+            var weaponBuilder = new StringBuilder();
+            var armourBuilder = new StringBuilder();
+            var itemBuilder = new StringBuilder();
+
+            foreach (var inv in characterInventory)
+            {
+                if (inv.ValkFinderArmour == null && inv.ValkFinderItem == null)
+                {
+                    weaponBuilder.AppendLine($"{inv.ValkFinderWeapon.WeaponName}: {inv.Amount}");
+                }
+                else if(inv.ValkFinderWeapon == null && inv.ValkFinderItem == null)
+                {
+                    armourBuilder.AppendLine($"{inv.ValkFinderArmour.ArmourName}: {inv.Amount}");
+                }
+                else if(inv.ValkFinderWeapon == null && inv.ValkFinderArmour == null)
+                {
+                    itemBuilder.AppendLine($"{inv.ValkFinderItem.ItemName}: {inv.Amount}");
+                }
+
+            }
+            if (weaponBuilder != null)
+            {
+                embedBuilder.AddField("Weapons:", weaponBuilder.ToString());
+            }
+            if (armourBuilder != null)
+            {
+                embedBuilder.AddField("Armour:", armourBuilder.ToString());
+            }
+            if (itemBuilder != null)
+            {
+                embedBuilder.AddField("Items:", itemBuilder.ToString());
+            }
+            if (embedBuilder.Fields == null)
+            {
+                embedBuilder.AddField("Empty", "Your Inventory is empty!");
+            }
+            return embedBuilder;
+        }
+
     }
 }
