d9709889d1b8042988a587157cc2aa5d78c65945
MrGrindor <54775642+MrGrindor@users.noreply.github.com>
2020-06-04 22:41:34 +0200

    Turned the StatToMod method into something that hopefully works for all natural numbers

--- Steward/Services/RollService.cs
+++ Steward/Services/RollService.cs
@@ -344,30 +344,7 @@ namespace Steward.Services
 
 		private static int StatToMod(int stat)
 		{
-			return stat switch
-			{
-				1 => -5,
-				2 => -4,
-				3 => -4,
-				4 => -3,
-				5 => -3,
-				6 => -2,
-				7 => -2,
-				8 => -1,
-				9 => -1,
-				10 => 0,
-				11 => 0,
-				12 => 1,
-				13 => 1,
-				14 => 2,
-				15 => 2,
-				16 => 3,
-				17 => 3,
-				18 => 4,
-				19 => 4,
-				20 => 5,
-				_ => 0
-			};
+			return Convert.ToInt32(Math.Floor((Convert.ToDouble(stat) - 10) / 2));
 		}
 	}
 }
